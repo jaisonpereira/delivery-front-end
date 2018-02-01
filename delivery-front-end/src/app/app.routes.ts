c30977476902ee865ce4222fe985d1919b9e4f26
Jaison Pereira <douglas>
2018-02-01 21:29:48 -0200

    order

--- delivery-front-end/src/app/app.routes.ts
+++ delivery-front-end/src/app/app.routes.ts
@@ -8,15 +8,17 @@ import { RestaurantsComponent } from './restaurants/restaurants.component'
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component'
 import { MenuComponent } from 'app/restaurant-detail/menu/menu.component';
 import { ReviewsComponent } from 'app/restaurant-detail/reviews/reviews.component';
+import { OrderComponent } from 'app/order/order.component';
 
 export const ROUTES: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AboutComponent },
     { path: 'restaurants', component: RestaurantsComponent },
+    { path: 'order', component: OrderComponent },
     {
         path: 'restaurants/:id', component: RestaurantDetailComponent,
         children: [
-            { path: '', redirectTo:'menu',pathMatch:'full' },
+            { path: '', redirectTo: 'menu', pathMatch: 'full' },
             { path: 'menu', component: MenuComponent },
             { path: 'reviews', component: ReviewsComponent }
         ]
