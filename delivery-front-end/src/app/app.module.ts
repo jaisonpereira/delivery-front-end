377e2e36129738488d78adead53de3cdca42f491
Jaison Pereira <douglas>
2018-02-07 21:54:45 -0200

    order itens

--- delivery-front-end/src/app/app.module.ts
+++ delivery-front-end/src/app/app.module.ts
@@ -21,7 +21,8 @@ import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.compo
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { OrderComponent } from './order/order.component';
 import { InputComponent } from './shared/input/input.component';
-import { RadioComponent } from './shared/radio/radio.component'
+import { RadioComponent } from './shared/radio/radio.component';
+import { OrderItensComponent } from './order/order-itens/order-itens.component'
 
 
 
@@ -40,7 +41,8 @@ import { RadioComponent } from './shared/radio/radio.component'
     ReviewsComponent,
     OrderComponent,
     InputComponent,
-    RadioComponent
+    RadioComponent,
+    OrderItensComponent
   ],
   imports: [
     BrowserModule,
